c277dbfc7a5c6053c31375e994ed74e6360f8700
Vladimir Vasenev <vovasenev@172.31.91.61-vpn.dhcp.yndx.net>
2025-12-05 11:34:38 +0300

    solution

--- src/sdf_main.glsl
+++ src/sdf_main.glsl
@@ -5,23 +5,68 @@ float sdSphere(vec3 p, float r)
     return length(p) - r;
 }
 
+float sdRoundCone( vec3 p, float r1, float r2, float h )
+{
+  float b = (r1-r2)/h;
+  float a = sqrt(1.0-b*b);
+
+  vec2 q = vec2( length(p.xz), p.y );
+  float k = dot(q,vec2(-b,a));
+  if( k<0.0 ) return length(q) - r1;
+  if( k>a*h ) return length(q-vec2(0.0,h)) - r2;
+  return dot(q, vec2(a,b) ) - r1;
+}
+
 // XZ plane
 float sdPlane(vec3 p)
 {
     return p.y;
 }
 
-// косинус который пропускает некоторые периоды, удобно чтобы махать ручкой не все время
-float lazycos(float angle)
+float opRound( in float p, in float rad )
 {
-    int nsleep = 10;
+    return p - rad;
+}
 
+vec4 sdVerticalCapsule( vec3 p, float h, float r )
+{
+    p.y -= clamp( p.y, 0.0, h );
+    return vec4(length( p ) - r, vec3(0.0, 1.0, 0.0));
+}
+
+vec4 sdCapsule( vec3 p, vec3 a, vec3 b, float r )
+{
+  vec3 pa = p - a, ba = b - a;
+  float h = clamp( dot(pa,ba)/dot(ba,ba), 0.0, 1.0 );
+  return vec4(length( pa - ba*h ) - r, vec3(0.0, 1.0, 0.0));
+}
+
+// косинус который пропускает некоторые периоды, удобно чтобы махать ручкой не все время
+float lazycos(float angle, int nsleep, int limit)
+{
     int iperiod = int(angle / 6.28318530718) % nsleep;
-    if (iperiod < 3) {
+    if (iperiod < limit) {
         return cos(angle);
     }
 
     return 1.0;
+}
+
+float lazysin(float angle, int nsleep, int limit)
+{
+    int iperiod = int(angle / 6.28318530718) % nsleep;
+    if (iperiod < limit) {
+        return sin(angle);
+    }
+
+    return 0.0;
+}
+
+float smin( float a, float b, float k )
+{
+    k *= log(2.0);
+    float x = b-a;
+    return a + x/(1.0-exp2(x/k));
 }
 
 // возможно, для конструирования тела пригодятся какие-то примитивы из набора https://iquilezles.org/articles/distfunctions/
@@ -31,7 +76,8 @@ vec4 sdBody(vec3 p)
     float d = 1e10;
 
     // TODO
-    d = sdSphere((p - vec3(0.0, 0.35, -0.7)), 0.35);
+    d = opRound(sdRoundCone((p - vec3(0.0, 0.4, -0.7)), 0.2, 0.1, 0.3), 0.18);
+    
 
     // return distance and color
     return vec4(d, vec3(0.0, 1.0, 0.0));
@@ -40,9 +86,27 @@ vec4 sdBody(vec3 p)
 vec4 sdEye(vec3 p)
 {
 
-    vec4 res = vec4(1e10, 0.0, 0.0, 0.0);
+    vec3 color = vec3(1.0, 1.0, 1.0);
+    
+    p = p - vec3(0.0, 0.67, -0.45);
+    if (length(p.xy) < 0.1) {
+        color = vec3(0.35, 1, 1);
+    }
+    if (length(p.xy) < 0.06) {
+        color = vec3(0.1, 0.1, 0.1);
+    }
+    
+    if (length(p.xz) < lazysin(iTime * 4., 4, 2)) {
+        color = vec3(0.1, 1.0, 0.1);
+    }
+
+    vec4 res = vec4(sdSphere(p, 0.18), color);
 
     return res;
+}
+
+vec4 opUnion(vec4 a, vec4 b) {
+    return a.x < b.x ? a : b;
 }
 
 vec4 sdMonster(vec3 p)
@@ -52,11 +116,24 @@ vec4 sdMonster(vec3 p)
     p -= vec3(0.0, 0.08, 0.0);
 
     vec4 res = sdBody(p);
-
     vec4 eye = sdEye(p);
-    if (eye.x < res.x) {
-        res = eye;
-    }
+    vec4 l_leg = sdVerticalCapsule((p - vec3(0.1, 0.0, -0.7)), 0.05, 0.06);
+    vec4 r_leg = sdVerticalCapsule((p - vec3(-0.1, 0.0, -0.7)), 0.05, 0.06);
+    vec4 l_arm = sdCapsule((p - vec3(0.4, 0.0, -0.7)), vec3(-0.1, 0.5, 0.0), vec3(0.03, 0.3, 0.0), 0.05);
+    
+    vec3 dir = vec3(-0.03, 0.3, 0.0) - vec3(0.1, 0.5, 0.0);
+    float new_y = dir.y * lazycos(10. * iTime, 10, 3) - dir.z * lazysin(10. * iTime, 10, 3);
+    float new_z = dir.y * lazysin(10. * iTime, 10, 3) + dir.z * lazycos(10. * iTime, 10, 3);
+    dir.y = new_y;
+    dir.z = new_z;
+    vec4 r_arm = sdCapsule((p - vec3(-0.4, 0.0, -0.7)), vec3(0.1, 0.5, 0.0), vec3(0.1, 0.5, 0.0) + dir, 0.05);
+    
+    // res.x = smin(res.x, eye.x, 0.01);
+    res = opUnion(res, eye);
+    res.x = smin(res.x, l_leg.x, 0.005);
+    res.x = smin(res.x, r_leg.x, 0.005);
+    res.x = smin(res.x, l_arm.x, 0.01);
+    res.x = smin(res.x, r_arm.x, 0.01);
 
     return res;
 }
